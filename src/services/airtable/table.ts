aebe2f2c6a70e849f380c140db959981d8fcb732
Guillaume Piedigrossi <guillaume.piedigrossi@gadz.org>
2021-09-22 15:16:12 +0200

    :sparkles: (table) connect to the Clean Database

--- src/services/airtable/table.ts
+++ src/services/airtable/table.ts
@@ -3,4 +3,4 @@ import Airtable from "airtable";
 export const table = new Airtable({
   apiKey: process.env.AIRTABLE_API_KEY,
   endpointUrl: "https://api.airtable.com",
-}).base(process.env.AIRTABLE_DB_ID || "")("Database");
+}).base(process.env.AIRTABLE_DB_ID || "")("Clean Database");
