8b0cabc814d2ef0c11bab6a62304e86fab820c58
Skander Ellouze <skandere@bam.tech>
2022-07-11 17:37:09 +0200

    feat: add a legend to the chart + refacto the graph component

---
+++ src/modules/bdc/views/Chart.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ReferenceLine,
+  XAxis,
+  YAxis,
+} from "recharts";
+import { formatDate } from "../../../../src/utils/formatDate";
+import { ChartPoint } from "../../../types/charts";
+import { ReleaseDTO } from "../../project/types";
+import { parseISO } from "date-fns";
+import { ChartLegend } from "./ChartLegend";
+
+export const DONE_STROKE_COLOR = "#0059ff";
+export const STANDARD_STROKE_COLOR = "#ff0000";
+
+export const Chart = ({
+  data,
+  sortedReleases,
+}: {
+  data: ChartPoint[];
+  sortedReleases: ReleaseDTO[];
+}) => {
+  return (
+    <LineChart width={800} height={400} data={data} id="bdc">
+      <Legend
+        verticalAlign={"bottom"}
+        wrapperStyle={{
+          display: "flex",
+          alignItems: "flex-end",
+          justifyContent: "center",
+          bottom: "-10px",
+        }}
+        content={() => <ChartLegend />}
+      />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <CartesianGrid stroke="#ccc" />
+
+      <Line type="linear" stroke={DONE_STROKE_COLOR} dataKey="done" />
+      <Line
+        type="linear"
+        stroke={DONE_STROKE_COLOR}
+        dataKey="forecast"
+        strokeDasharray="5 5"
+      />
+      <Line type="linear" stroke={STANDARD_STROKE_COLOR} dataKey="standard" />
+      {sortedReleases.map((release, index) => {
+        if (index !== 0) {
+          return (
+            <ReferenceLine
+              x={formatDate(parseISO(release.createdAt))}
+              stroke={"#0059ff"}
+              key={release.id}
+              label={release.name}
+              strokeDasharray="5 5"
+            />
+          );
+        }
+      })}
+    </LineChart>
+  );
+};
