e54feed016e34b6275891064152957204c697708
Guillaume Piedigrossi <guillaume.piedigrossi@gadz.org>
2022-08-05 16:09:44 +0200

    :sparkles: (makeVersionChart) display forecast up to the date where all the scope is reached

--- src/modules/bdc/helpers/getDoneChartPoints.ts
+++ src/modules/bdc/helpers/getDoneChartPoints.ts
@@ -2,25 +2,29 @@ import { addBusinessDays, differenceInBusinessDays, parseISO } from "date-fns";
 import { range } from "lodash";
 import { DoneChartPoint } from "../../../types/charts";
 import { formatDate } from "../../../utils/formatDate";
-import { FullProjectDTO } from "../../project/types";
+import { FullProjectDTO, VersionDTO } from "../../project/types";
 import { computeVolumeEstimationFromDate } from "./computeVolumeEstimationFromDate";
 
 export const getDoneChartPoints = ({
   project,
   startDate,
   endDate,
+  version,
 }: {
   project: FullProjectDTO;
   startDate: Date;
   endDate: Date;
+  version: VersionDTO;
 }): DoneChartPoint[] => {
   const numberOfDays = differenceInBusinessDays(endDate, startDate) + 1;
 
   let cumulativeDone = 0;
   let cumulativeForecastDone = 0;
+  let cumulativeDay = 0;
   const doneChartPoints: DoneChartPoint[] = [];
 
   range(numberOfDays).forEach((currentDayIdx) => {
+    cumulativeDay = currentDayIdx;
     const currentDayDone = project.productions.find(
       (production) =>
         formatDate(parseISO(production.date)) ===
@@ -46,6 +50,25 @@ export const getDoneChartPoints = ({
         )
       : cumulativeDone;
   });
+
+  // extend the chart up to the forecast is equal to 0, to forecast the date of landing
+  while (
+    doneChartPoints[doneChartPoints.length - 1].forecastDone <
+    version.releases[version.releases.length - 1].volume
+  ) {
+    cumulativeDay += 1;
+
+    cumulativeForecastDone += computeVolumeEstimationFromDate(
+      addBusinessDays(startDate, cumulativeDay),
+      project
+    );
+
+    doneChartPoints.push({
+      name: formatDate(addBusinessDays(startDate, cumulativeDay)),
+      done: NaN,
+      forecastDone: cumulativeForecastDone,
+    });
+  }
 
   return doneChartPoints;
 };
